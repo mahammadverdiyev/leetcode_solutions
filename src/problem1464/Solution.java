c48842085f3d60528b49327e3221d4b167950dcb
Mahammad <devmahammad@gmail.com>
2021-09-25 20:05:12 +0400

    update

---
+++ src/problem1464/Solution.java
@@ -0,0 +1,11 @@
+package problem1464;
+
+import java.util.Arrays;
+
+public class Solution
+{
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+        return (nums[nums.length - 1] - 1) * (nums[nums.length - 2] - 1);
+    }
+}
--- src/problem1828/Solution.java
+++ src/problem1828/Solution.java
@@ -1,10 +1,13 @@
 package problem1828;
 
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 class Point
 {
